a5e491ade7d2857f92f7ce7bac045898aee5218a
Pauline Saveliev <paulinesaveliev@yahoo.com>
2024-02-27 17:01:32 -0500

    completed in class activity

--- src/quiz/promise-neg.js
+++ src/quiz/promise-neg.js
@@ -3,3 +3,13 @@ const array2D = [
     [4, 5, 6],
     [7, 8, -9]
 ];
+
+function negsPerRow(arr, rowInd) {
+    return new Promise((resolve, reject) => {
+        // find if there is neg in the array at the rowInd
+    });
+}
+
+Promise.any(negsPerRowPromises)
+.then((results) => {console.log(results);})
+.catch((error) => console.log(`error msg: ${error}`));
--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -3,3 +3,37 @@ const array2D = [
     [4, 5, 6],
     [7, 8, 9]
 ];
+
+function sum2DArray(arr) {
+    return new Promise((resolve, reject) => {
+        console.log('Sum called ... ');
+        if(Array.isArray(arr)) {
+            setTimeout(() => {
+                let sum = 0;
+                for (let i = 0; i < arr.length; i++) {
+                    for (let j = 0; j < arr[i].length; j++) {
+                        sum += arr[i][j];
+                    }
+                }
+                console.log('resolving ... ');
+                resolve(sum);
+            }, 0);
+        }
+        else {
+            console.log('rejecting ... ');
+            reject('BAD INPUT: Expected array as input');
+        }
+        console.log('returning from sum');
+    });
+}
+
+const result1 = sum2DArray(array2D)
+const result2 = sum2DArray('beef')
+
+Promise.any([result1, result2])
+.then((response) => {
+    console.log(`${response.url}: ${response.status}`);
+})
+.catch((error) => {
+    console.error(`Failed to fetch: ${error}`);
+});
